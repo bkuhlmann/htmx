ea18e604b70503358a3157ba24019adc40904f6b
Brooke Kuhlmann <brooke@alchemists.io>
2023-09-30 15:47:18 -0600

    Fixed Zeitwerk loader
    Necessary to ensure the gem is properly tagged and the default inflector is used since the gem inflector unfortunately looks for a version file which is not used by this gem.
    
    Milestone: patch

--- lib/htmx.rb
+++ lib/htmx.rb
@@ -2,8 +2,10 @@
 
 require "zeitwerk"
 
-Zeitwerk::Loader.for_gem.then do |loader|
+Zeitwerk::Loader.new.then do |loader|
   loader.inflector.inflect "htmx" => "HTMX"
+  loader.tag = File.basename __FILE__, ".rb"
+  loader.push_dir __dir__
   loader.setup
 end
 
