35cd3d0f9607cd26239dc10706a4b214c79f0748
Brooke Kuhlmann <brooke@alchemists.io>
2023-11-14 20:48:10 -0700

    Fixed gem loader to find by tag and cache instance
    Necessary to fix situations where you want to acquire the loader in other projects. In this case, the loader is acquired by unique tag and then cached.
    
    Additionally specs have been added for proper coverage.
    
    Milestone: patch

--- lib/htmx.rb
+++ lib/htmx.rb
@@ -11,7 +11,9 @@ end
 
 # Main namespace.
 module HTMX
-  def self.loader(registry = Zeitwerk::Registry) = registry.loader_for __FILE__
+  def self.loader registry = Zeitwerk::Registry
+    @loader ||= registry.loaders.find { |loader| loader.tag == File.basename(__FILE__, ".rb") }
+  end
 
   def self.[](...)
     @prefixer ||= Prefixer.new
--- spec/lib/htmx_spec.rb
+++ spec/lib/htmx_spec.rb
@@ -3,6 +3,17 @@
 require "spec_helper"
 
 RSpec.describe HTMX do
+  describe ".loader" do
+    it "eager loads" do
+      expectation = proc { described_class.loader.eager_load force: true }
+      expect(&expectation).not_to raise_error
+    end
+
+    it "answers unique tag" do
+      expect(described_class.loader.tag).to eq("htmx")
+    end
+  end
+
   describe ".[]" do
     it "answers prefixed attributes" do
       attributes = described_class["get" => "/tasks", trigger: "click"]
